e1b2b78f1ac972624e9e481de528507b229a42a5
teknokolik61 <info@miracozturk.com.tr>
2024-03-24 18:09:31 +0300

    Update sophos_ayar.bat

--- dosyalar/sophos_ayar.bat
+++ dosyalar/sophos_ayar.bat
@@ -3,7 +3,8 @@
 cd %SYSTEMDRIVE%\Bilgisayarim\bin\
 del sophos.bat
 %SYSTEMDRIVE%\Bilgisayarim\Bin\wget.exe https://raw.githubusercontent.com/teknokolik61/Bilgisayarim-Client/main/dosyalar/Basterzi_Vpn_04.02.2024.tgb
-
+pause
+pause
 
 echo ============================================================================================
 set yy=%date:~-4%
@@ -29,7 +30,8 @@ call :Color 6 "  [1] AYARLARINI IMPORT ETMEK ICIN" &echo:
 echo.
 echo ============================================================================================
 echo.
-
+pause
+pause
 ::===============================================================================================================
 :Color_Pre
 for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (set "DEL=%%a") &exit /b
