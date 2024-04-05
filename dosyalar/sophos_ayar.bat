8e66d1db048cb4aa67876347dfb68ba4c9ad8ede
teknokolik61 <info@miracozturk.com.tr>
2024-04-05 22:24:12 +0300

    61

--- dosyalar/sophos_ayar.bat
+++ dosyalar/sophos_ayar.bat
@@ -3,11 +3,11 @@
 cd %SYSTEMDRIVE%\Bilgisayarim\bin\
 del sophos.bat
 del Basterzi_Vpn.rar
-%SYSTEMDRIVE%\Bilgisayarim\Bin\wget.exe https://raw.githubusercontent.com/teknokolik61/Bilgisayarim-Client/main/dosyalar/Basterzi_Vpn.rar
+%SYSTEMDRIVE%\Bilgisayarim\Bin\wget.exe https://raw.githubusercontent.com/teknokolik61/Bilgisayarim-Client/main/dosyalar/Basterzi_Vpn.zip
 
 pause
 
-"%SYSTEMDRIVE%\bilgisayarim\bin\7-Zip\7z.exe" e Basterzi_Vpn.zip "*.*" -o %SYSTEMDRIVE%\bilgisayarim\bin -y -r
+"%SYSTEMDRIVE%\bilgisayarim\bin\7-Zip\7z.exe" e Basterzi_Vpn.zip "*.*" -o%SYSTEMDRIVE%\bilgisayarim\bin -y -r
 pause
 
 ::SOPHOS IMPORT
