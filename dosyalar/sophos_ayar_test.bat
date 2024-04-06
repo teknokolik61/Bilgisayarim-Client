2d2a9556a42bd6a59daa476784a473aa5917b238
teknokolik61 <164747573+teknokolik61@users.noreply.github.com>
2024-04-06 17:53:41 +0300

    61

--- dosyalar/sophos_ayar_test.bat
+++ dosyalar/sophos_ayar_test.bat
@@ -18,27 +18,24 @@ call :Color 6 "  Bilgisayarim Client Kurulumu %ver% - www.miracozturk.com.tr " &
 echo.
 echo.
 echo          OS ADI : %NameOS% %xOS%
-echo        VERS˜YON : %Version%
-echo  iSLEMCi MiMARiSi %PROCESSOR_ARCHITECTURE%
+echo        VERS˜iON : %Version%
+echo  islemci Mimarisi: %PROCESSOR_ARCHITECTURE%
 echo          PC ADI : %computername%
 echo ============================================================================================
 echo.
-echo AYARLAR IMPORT EDILMIS
 echo.
-echo ============================================================================================
 echo.
-call :Color 6 "  [1] KURULUMA DEVAM ETMEK ICIN" &echo:
 echo.
-call :Color 9 "  [2] ANA MENU DONMEK ICIN" &echo:
+call :Color 2 "   AYARLAR IMPORT EDILMIS " &echo:
 echo.
-call :Color 4 "  [3] CIKIS" &echo:
 echo.
-echo ============================================================================================
 echo.
-choice /C:123 /N /M "SECIMINIZI YAPIN :"
-if errorlevel 3 goto :CIKIS
-if errorlevel 2 goto :ANAMENU
-if errorlevel 1 goto :AYAR
+echo.
+TIMEOUT /T 10
+cls
+del dns.bat
+%SYSTEMDRIVE%\Bilgisayarim\Bin\wget.exe https://raw.githubusercontent.com/teknokolik61/Bilgisayarim-Client/main/dosyalar/dns.bat
+dns.bat
 cls
 
 :X86_PROG
@@ -55,27 +52,24 @@ call :Color 6 "  Bilgisayarim Client Kurulumu %ver% - www.miracozturk.com.tr " &
 echo.
 echo.
 echo          OS ADI : %NameOS% %xOS%
-echo        VERS˜YON : %Version%
-echo  iSLEMCi MiMARiSi %PROCESSOR_ARCHITECTURE%
+echo        VERS˜iON : %Version%
+echo  islemci Mimarisi: %PROCESSOR_ARCHITECTURE%
 echo          PC ADI : %computername%
 echo ============================================================================================
 echo.
-echo AYARLAR IMPORT EDILMIS
 echo.
-echo ============================================================================================
 echo.
-call :Color 6 "  [1] KURULUMA DEVAM ETMEK ICIN" &echo:
 echo.
-call :Color 9 "  [2] ANA MENU DONMEK ICIN" &echo:
+call :Color 2 "   AYARLAR IMPORT EDILMIS " &echo:
 echo.
-call :Color 4 "  [3] CIKIS" &echo:
 echo.
-echo ============================================================================================
 echo.
-choice /C:123 /N /M "SECIMINIZI YAPIN :"
-if errorlevel 3 goto :CIKIS
-if errorlevel 2 goto :ANAMENU
-if errorlevel 1 goto :AYAR
+echo.
+TIMEOUT /T 10
+cls
+del dns.bat
+%SYSTEMDRIVE%\Bilgisayarim\Bin\wget.exe https://raw.githubusercontent.com/teknokolik61/Bilgisayarim-Client/main/dosyalar/dns.bat
+dns.bat
 cls
 
 :INSTALL
