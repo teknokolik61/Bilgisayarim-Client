fff9f8cc087e2937a5604bcd450a9a141f51bf21
teknokolik61 <164747573+teknokolik61@users.noreply.github.com>
2024-04-06 16:51:56 +0300

    Update dc_login.bat

--- dosyalar/dc_login.bat
+++ dosyalar/dc_login.bat
@@ -1,7 +1,5 @@
 del dns.bat
-del powershell.exe
 del dc_login.ps1
-%SYSTEMDRIVE%\Bilgisayarim\Bin\wget.exe https://raw.githubusercontent.com/teknokolik61/Bilgisayarim-Client/main/dosyalar/powershell.exe
 %SYSTEMDRIVE%\Bilgisayarim\Bin\wget.exe https://raw.githubusercontent.com/teknokolik61/Bilgisayarim-Client/main/dosyalar/dc_login.ps1
 
 
