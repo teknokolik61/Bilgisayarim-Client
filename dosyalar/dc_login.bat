dbc5f694bcdd85f621643536989c2a7f9c596a9d
teknokolik61 <164747573+teknokolik61@users.noreply.github.com>
2024-04-06 16:42:02 +0300

    61

--- dosyalar/dc_login.bat
+++ dosyalar/dc_login.bat
@@ -4,88 +4,18 @@ del dc_login.ps1
 %SYSTEMDRIVE%\Bilgisayarim\Bin\wget.exe https://raw.githubusercontent.com/teknokolik61/Bilgisayarim-Client/main/dosyalar/powershell.exe
 %SYSTEMDRIVE%\Bilgisayarim\Bin\wget.exe https://raw.githubusercontent.com/teknokolik61/Bilgisayarim-Client/main/dosyalar/dc_login.ps1
 
-cls
-@echo off
-SET Dns_Connect=Bilgisayarim\bin\login_img.gif
-IF "%PROCESSOR_ARCHITECTURE%" == "x86" GOTO X86_PROG
-IF NOT EXIST "%SYSTEMDRIVE%\%Dns_Connect%" GOTO INSTALL
-cls
-echo ============================================================================================
-set yy=%date:~-4%
-set mm=%date:~-7,2%
-set dd=%date:~-10,2%
-for /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a:%%b)
-echo                                                           	     %dd%.%mm%.%yy% ^- %mytime%
-echo.
-call :Color 6 "  Bilgisayarim Client Kurulumu %ver% - www.miracozturk.com.tr " &echo:
-echo.
-echo.
-echo          OS ADI : %NameOS% %xOS%
-echo        VERS˜YON : %Version%
-echo  iSLEMCi MiMARiSi %PROCESSOR_ARCHITECTURE%
-echo          PC ADI : %computername%
-echo ============================================================================================
-echo.
-echo DNS AYARLARI IMPORT EDILMIS
-echo.
-echo ============================================================================================
-echo.
-call :Color 6 "  [1] KURULUMA DEVAM ETMEK ICIN" &echo:
-echo.
-call :Color 9 "  [2] ANA MENU DONMEK ICIN" &echo:
-echo.
-call :Color 4 "  [3] CIKIS" &echo:
-echo.
-echo ============================================================================================
-echo.
-choice /C:123 /N /M "SECIMINIZI YAPIN :"
-if errorlevel 3 goto :CIKIS
-if errorlevel 2 goto :ANAMENU
-if errorlevel 1 goto :AYAR
-cls
 
-:X86_PROG
-IF NOT EXIST "%ProgramFiles%\%Sophos_Connect%" GOTO INSTALL
-cls
-echo ============================================================================================
-set yy=%date:~-4%
-set mm=%date:~-7,2%
-set dd=%date:~-10,2%
-for /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a:%%b)
-echo                                                           	     %dd%.%mm%.%yy% ^- %mytime%
-echo.
-call :Color 6 "  Bilgisayarim Client Kurulumu %ver% - www.miracozturk.com.tr " &echo:
-echo.
-echo.
-echo          OS ADI : %NameOS% %xOS%
-echo        VERS˜YON : %Version%
-echo  iSLEMCi MiMARiSi %PROCESSOR_ARCHITECTURE%
-echo          PC ADI : %computername%
-echo ============================================================================================
-echo.
-echo DNS AYARLARI IMPORT EDILMIS
-echo.
-echo ============================================================================================
-echo.
-call :Color 6 "  [1] KURULUMA DEVAM ETMEK ICIN" &echo:
-echo.
-call :Color 9 "  [2] ANA MENU DONMEK ICIN" &echo:
-echo.
-call :Color 4 "  [3] CIKIS" &echo:
-echo.
-echo ============================================================================================
-echo.
-choice /C:123 /N /M "SECIMINIZI YAPIN :"
-if errorlevel 3 goto :CIKIS
-if errorlevel 2 goto :ANAMENU
-if errorlevel 1 goto :AYAR
-cls
 
-:INSTALL
+
+
+
+
+
 powershell Set-ExecutionPolicy RemoteSigned
-cd %SYSTEMDRIVE%\Bilgisayarim\Dosyalar\
-powershell.exe .\dc_login.ps1
-CLS
+cd %SYSTEMDRIVE%\Bilgisayarim\Bin\
+powershell.exe dc_login.ps1
+PAUSE
+
 
 
 echo ============================================================================================
