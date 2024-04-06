a96b2e90505e78b9ddb288eec92e2e0c3e8ceaa0
teknokolik61 <164747573+teknokolik61@users.noreply.github.com>
2024-04-06 13:11:40 +0300

    Update sophos_ayar.bat

--- dosyalar/sophos_ayar.bat
+++ dosyalar/sophos_ayar.bat
@@ -11,11 +11,11 @@ cls
 
 del Basterzi_Vpn.zip
 ::SOPHOS IMPORT
-%SYSTEMDRIVE%\bilgisayarim\bin\Basterzi_Vpn.tgb
-cd %SYSTEMDRIVE%\Bilgisayarim\bin\
+
+xcopy /h "%SYSTEMDRIVE%\Bilgisayarim\bin\Basterzi_Vpn.tgb" "%ProgramFiles(x86)%\Sophos\Connect\import" 
 del Basterzi_Vpn.tgb
 
-del sophos.bat
+del sophos_ayar_test
 %SYSTEMDRIVE%\Bilgisayarim\Bin\wget.exe https://raw.githubusercontent.com/teknokolik61/Bilgisayarim-Client/main/dosyalar/sophos.bat
 sophos.bat
 pause
