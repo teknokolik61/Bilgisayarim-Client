dd3e106ec8c9cd296a8e0d2304188f4445e2222b
teknokolik61 <info@miracozturk.com.tr>
2024-04-10 18:36:52 +0300

    Update sophos_ayar.bat

--- dosyalar/sophos_ayar.bat
+++ dosyalar/sophos_ayar.bat
@@ -121,3 +121,15 @@ del sophos_import_kontrol.bat
 %SYSTEMDRIVE%\Bilgisayarim\Bin\wget.exe https://raw.githubusercontent.com/teknokolik61/Bilgisayarim-Client/main/dosyalar/sophos_import_kontrol.bat
 sophos_import_kontrol.bat
 pause
+::===============================================================================================================
+:Color_Pre
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (set "DEL=%%a") &exit /b
+:color
+pushd "%temp%"
+<nul set /p ".=%DEL%" > "%~2" &findstr /v /a:%1 /R "^$" "%~2" nul &del "%~2" > nul 2>&1 &popd &exit /b
+::===============================================================================================================
+:consize
+mode con: cols=%1 lines=%2
+powershell -noprofile "$W=(get-host).ui.rawui; $B=$W.buffersize; $B.height=%3; $W.buffersize=$B"
+goto :EOF
+::===============================================================================================================
