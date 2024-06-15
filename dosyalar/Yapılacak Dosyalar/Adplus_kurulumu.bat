bcfa70411645a71035c5da0cc0c872c2774e12fe
teknokolik61 <info@miracozturk.com.tr>
2024-06-16 01:06:42 +0300

    61

---
+++ dosyalar/Yapılacak Dosyalar/Adplus_kurulumu.bat
@@ -0,0 +1,1 @@
+msiexec /i "ADSelfServicePlusClientSoftware.msi" /QN SERVERNAME="adplussrv.bilgisayarim.local" PROTOCOL="http" PORTNO="8888" INSTALLATION_KEY="8iLwasToyffByfCQZy32wP74hK7i4VfjAV8TI3CL" LOCALE_ID="tr"
